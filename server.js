877b18bf9335bf55280253930cac8b12cdb9cd42
Michel <michelmdes@gmail.com>
2020-11-16 02:22:56 -0300

    Add config servidor Heroku 2

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Importa as dependencias que acabamos de instalar
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve os arquivos estaticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/pedido-medico-front'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/pedido-medico-front/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-// Inicia a aplicacao pela porta configurada
 app.listen(process.env.PORT || 8080);
