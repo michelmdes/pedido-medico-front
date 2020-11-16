6390c86b202c17591c69a4bea3a7c3241705107b
Michel <michelmdes@gmail.com>
2020-11-16 02:34:01 -0300

    Add config servidor Heroku 4

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,4 +1,3 @@
 import {environment} from '../environments/environment'
 
-export const BANCO_API = environment.banco
 export const PEDIDO_EXAME_API = environment.api
--- src/app/pedido-exames/pedido-exames.service.ts
+++ src/app/pedido-exames/pedido-exames.service.ts
@@ -32,22 +32,4 @@ export class PedidoExamesService {
       .catch(ErrorHandler.handleError);
   }
 
-    /*restaurantById(id: string): Observable<Restaurant> {
-      return this.http.get(`${PEDIDO_EXAME_API}/restaurants/${id}`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError);
-    }*/
-
-    /*reviewsOfRestaurant(id: string): Observable<any> {
-      return this.http.get(`${BANCO_API}/restaurants/${id}/reviews`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError);
-    }
-
-    menuOfRestaurant(id: string): Observable<MenuItem[]> {
-      return this.http.get(`${BANCO_API}/restaurants/${id}/menu`)
-        .map(response => response.json())
-        .catch(ErrorHandler.handleError);
-    }*/
-
 }
