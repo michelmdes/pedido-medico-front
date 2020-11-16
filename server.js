39a4be2fe115462c1a875a9c2d3274b8689fd8e3
Michel <michelmdes@gmail.com>
2020-11-16 11:17:10 -0300

    Config Heroku

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
-});
-
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
 
 console.log('Console listening!');
